fa5f7e9cd8a158875d1fa8afb069d73b4a40b51e
Evan Jacobs <evanjacobs@yahoo.com>
2013-02-02 14:02:59 -0800

    remove keys from app

--- squeeze.rb
+++ squeeze.rb
@@ -4,9 +4,9 @@ require 'delayed_job'
 require 'delayed_job_active_record'
 require 'pusher'
 
-Pusher.app_id = 36617
-Pusher.key = 'c1c5221d995f9bec3010'
-Pusher.secret = '09d04d8fea0a1a6769ea'
+Pusher.app_id = ENV['PUSHER_APP_ID']
+Pusher.key = ENV['PUSHER_KEY']
+Pusher.secret = ENV['PUSHER_SECRET']
 
 get '/' do
   erb :index
